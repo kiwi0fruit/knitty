adc1c5785af81b687fb8f209f72c86cab0140581
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-14 01:32:22 +0700

    fix where function

--- knitty/tools/tools.py
+++ knitty/tools/tools.py
@@ -40,35 +40,52 @@ def strict_str(smth) -> str:
     return smth if smth and isinstance(smth, str) else ''
 
 
-def where(executable: str, search_dirs: Iterable[str]=None) -> str:
+def where(executable: str, search_dirs_: Iterable[str]=None, exe_only: bool=True) -> str:
     """
-    :param executable: exec name without .exe
-    :param search_dirs: extra dirs to look for executables
+    :param executable: exec name without .exe, .bat or .cmd
+    :param search_dirs_: extra dirs to look for executables
+    :param exe_only: on Windows search for executable.exe only.
+      If False then search for .exe .bat .cmd (in this order).
     :return: On Windows: absolute path to the exec that was found
       in the search_dirs or in the $PATH.
       On Unix: absolute path to the exec that was found in the search_dirs
-      or executable arg unchanged.
+      or shutil.which('executable') if it is not None.
+      If wasn't found raises PandotoolsError.
     """
-    def exe(_exe): return f'{_exe}.exe' if (os.name == 'nt') else _exe
-    def is_exe(_exe): return True if (os.name == 'nt') else os.access(_exe, os.X_OK)
+    if not (os.name == 'nt'):
+        extensions = ('',)
+    elif exe_only:
+        extensions = ('.exe',)
+    else:
+        extensions = ('.exe', '.bat', '.cmd')
+    if not search_dirs_:
+        search_dirs_ = ()
 
-    for _dir in (search_dirs if search_dirs else ()):
-        _exec = p.normpath(p.join(_dir, exe(executable)))
-        if p.isfile(_exec):
-            if is_exe(_exec):
-                return p.abspath(_exec)
+    for _dir in search_dirs_:
+        for ext in extensions:
+            exe = p.abspath(p.normpath(p.join(_dir, f'{executable}{ext}')))
+            if p.isfile(exe):
+                if os.name == 'nt':
+                    return exe
+                elif os.access(exe, os.X_OK):
+                    return exe
 
     if os.name == 'nt':
         from subprocess import run, PIPE
-
-        exec_abs = run(
-            [p.expandvars(r'%WINDIR%\System32\where.exe'), f'$PATH:{executable}.exe'],
-            stdout=PIPE, encoding='utf-8',
-        ).stdout.split('\n')[0].strip('\r')
-
-        if p.isfile(exec_abs):
-            return exec_abs
-        else:
-            raise KnittyError(f"'{executable}' wasn't found in the {search_dirs} and in the $PATH.")
+        for ext in extensions:
+            exe = f'{executable}{ext}'.lower()
+            exe_abs = run([p.expandvars(r'%WINDIR%\System32\where.exe'), f'$PATH:{exe}'],
+                          stdout=PIPE, stderr=PIPE)
+            if not exe_abs.stderr:
+                execs = list(filter(lambda s: s.endswith(exe),
+                                    exe_abs.stdout.decode().lower().splitlines()))
+                if execs:
+                    if p.isfile(execs[0]):
+                        return execs[0]
     else:
-        return executable
+        from shutil import which
+        exe = which(executable)
+        if exe:
+            return exe
+    raise KnittyError(
+        f"'{executable}' (should be without extension) wasn't found in the [{', '.join(search_dirs_)}] and in the $PATH.")
