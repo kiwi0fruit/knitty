4447d8e8e307914c5ce7bdee05e25be5a436a318
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-03-13 22:40:22 +0700

    no notebook module bugfix

--- setup.py
+++ setup.py
@@ -40,11 +40,11 @@ setup(
     keywords='atom hydrogen jupyter pandoc markdown report',
     packages=find_packages(exclude=['docs', 'tests']),
 
-    install_requires=['jupyter_core', 'traitlets', 'ipython', 'jupyter_client',
-                      'nbconvert>=5.4.1', 'pandocfilters', 'py-pandoc>=2.6', 
-                      'click', 'psutil', 'panflute>=1.11.2', 'ipykernel',
-                      'nbformat', 'pandoc-attrs', 'pyyaml', 'shutilwhich-cwdpatch>=0.1.0'],
-    # jupyter_core traitlets ipython jupyter_client nbconvert pandocfilters "py-pandoc>=2.6" click psutil "panflute>=1.11.2" nbformat pandoc-attrs pyyaml "shutilwhich-cwdpatch>=0.1.0" ipykernel
+    install_requires=['jupyter_core', 'traitlets', 'ipython', 'jupyter_client', 'ipykernel',
+                      'nbconvert>=5.4.1', 'pandocfilters', 'py-pandoc>=2.6',
+                      'click', 'psutil', 'panflute>=1.11.2', 'shutilwhich-cwdpatch>=0.1.0',
+                      'nbformat', 'pandoc-attrs', 'pyyaml', 'notebook'],
+    # jupyter_core traitlets ipython jupyter_client nbconvert pandocfilters "py-pandoc>=2.6" click psutil "panflute>=1.11.2" nbformat pandoc-attrs pyyaml "shutilwhich-cwdpatch>=0.1.0" ipykernel notebook
     python_requires='>=3.6',
     extras_require={
         'dev': ['pytest', 'pytest-cov', 'pandas', 'matplotlib', 'sphinx', 'sphinx_rtd_theme', 'ghp-import',  # for stitch
